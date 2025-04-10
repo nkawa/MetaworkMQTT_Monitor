8a65d5c33d33eb84b967fec4b9b601505544c7e0
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2025-04-10 20:52:27 +0900

    update metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./globals.css";
 
 export const metadata = {
-  title: "Menroll",
+  title: "MetaworkMQTT Monitor",
   description: "Created by Nagoya Univ. UCLab",
 };
 
